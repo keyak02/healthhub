fdd638ef8a3b6c469c535bb3dd15a97f84e0f694
Mohammad Nizam Uddin <nizam@uni-koblenz.de>
2017-03-25 20:06:34 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,444 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title> HealthHUB </title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/kanso.min.css">
+
+	<!-- Main stylesheet -->
+	<link rel="stylesheet" href="css/styles.css">
+	<!-- GOOGLE FONT -->
+	<link href='http://fonts.googleapis.com/css?family=Lato:300' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Abel' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Raleway:300' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Quicksand' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Poiret+One' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Play' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Arimo' rel='stylesheet' type='text/css'>
+	<!-- Custom Font awesome -->
+	<link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+	<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+
+</head>
+<body>
+
+<div class="mynav">
+  <nav class="navbar navbar-inverse navbar-fixed-top border-bottom-shadow">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar3">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#"><img src="img/healthhub2.png" alt="Dispute Bills" >
+        </a>
+      </div>
+      <div id="navbar3" class="navbar-collapse collapse">
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Find a Doctor</a></li>
+          <li><a href="#">Find a Hospital</a></li>
+					<li><a href="#">Contact us</a></li>
+					<li><a href="#">About us</a></li>
+          <!-- <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+            <ul class="dropdown-menu" role="menu">
+              <li><a href="#">Action</a></li>
+              <li><a href="#">Another action</a></li>
+              <li><a href="#">Something else here</a></li>
+              <li class="divider"></li>
+              <li class="dropdown-header">Nav header</li>
+              <li><a href="#">Separated link</a></li>
+              <li><a href="#">One more separated link</a></li>
+            </ul>
+          </li> -->
+        </ul>
+      </div>
+      <!--/.nav-collapse -->
+    </div>
+    <!--/.container-fluid -->
+  </nav>
+</div>
+<!-- HEADER NAVBAR -->
+
+
+<!-- jumbotron search -->
+
+<div class="jumbotron lifesaver">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-sm-12" style="text-align:center;">
+
+				<h1 style="font-family: 'Quicksand'; color:#F4D27A;">Find A Life Saver</h1>
+				<h4 >You can find donors from all over the country by using our search filter</h4>
+				<img src="img/heart.png" alt="">
+
+			</div><!-- col-sm-12 -->
+		</div><!-- row-->
+
+		<div class="row">
+
+			<div class="col-sm-12" >
+				<form role="form-horizontal">
+                    <div class="col-lg-4">
+                    	<label for="">District</label>
+                        <select class="form-control input-lg">
+                          <option>Dhaka</option>
+                          <option>Khulna</option>
+                          <option>Rajshahi</option>
+                          <option>Barisal</option>
+                          <option>Sylhet</option>
+                          <option>Rangpur</option>
+                        </select>
+                    </div>
+                    <div class="col-lg-4">
+                    	<label for="">City</label>
+                        <select class="form-control input-lg">
+                          <option>Mohammadpur</option>
+                          <option>Dhanmondi</option>
+                          <option>Shahbag</option>
+                          <option>Ramna</option>
+                          <option>Badda</option>
+                        </select>
+
+                    </div>
+                    <div class="col-lg-4">
+                    	<label for="">Blood Group</label>
+                    	<select class = "form-control input-lg" >
+                    		<option value="a+">A+</option>
+                    		<option value="a-">A-</option>
+                    		<option value="b+">B+</option>
+                    		<option value="b-">B-</option>
+                    		<option value="ab+">AB+</option>
+                    		<option value="o+">O+</option>
+                    	</select>
+                    </div>
+			</div><!-- col-sm-12 -->
+		</div><!-- row -->
+		<div class="row" style="text-align:center; margin-top:10px;">
+					<input type="submit" class="button is-danger" value="SEARCH">
+				</form>
+		</div>
+
+
+	</div><!-- container -->
+
+</div><!-- jumbotron end -->
+
+
+
+
+<!-- jumbotron search end -->
+
+
+
+<!-- Jumbotron Call to action  -->
+
+<div class="jumbotron" style = "background-color: #ebeaed; color: #333333; margin-bottom:0px;">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12 " style="text-align:center;">
+				<div>
+					<h1 style="font-family: 'Abel'; color:#0A4958;"> Have a Heart </h1>
+					<h3 style = "/*color:#ffffff*/">Donate Blood <i class="fa fa-tint" style = "color:#D23641" ></i></h3>
+					<p style = "text-align:center; font-family: 'Lato'; font-size:16px;" >
+						Your one drop blood can make a huge difference.
+						Let's come forward. Donate blood willingly and </br>
+						raise awareness on blood donation.
+					</p>
+					</br>
+					<p>
+						<a href="#registration" class="button is-danger  ">Register as a blood donor</a>
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<!-- Jumbotron Call to action  -->
+
+<!--  Jumbotron What others say about healthhub-->
+<div class="jumbotron review">
+	<div class="container-fluid">
+		<div class="row" style="text-align:center; margin-bottom:20px;">
+			<h3> Appreciation </h3>
+
+		</div>
+		<div class="row">
+			<div class="col-md-4 col-md-offset-2 verticalLine">
+			  <div class="row" style="margin-top:50px;">
+				  <div class="col-md-1">
+				    <img src="img/patient.png" alt="patient" class=" img-circle " >
+
+				  </div>
+			  <div class="col-md-10">
+			    <div class="speech">
+			      <!-- <div class="point"></div> -->
+			        <h5 style = "text-align:justify; font-family: 'Raleway'; color: #0A4958;">Thanks to wonderful people who give blood. I lead a full and active life and am looking forward to getting married next year.</h5>
+			        <h5 class="patient-name">Nick Carter</h5>
+			     </div>
+			   </div>
+			  </div>
+			</div><!-- col-md-6 -->
+			<div class="col-md-4 col-md-offset-1 ">
+			  <div class="row" style="margin-top:50px;">
+				  <div class="col-md-1">
+				    <img src="img/patient.png" alt="patient" class=" img-circle " >
+
+				  </div>
+			  <div class="col-md-10">
+			    <div class="speech">
+			      <div class="point"></div>
+			        <h5 style = "text-align:justify; font-family: 'Raleway'; color: #0A4958;">Thanks to wonderful people who give blood. I lead a full and active life and am looking forward to getting married next year.</h5>
+			        <h5 class="patient-name">Nick Carter</h5>
+			     </div>
+			   </div>
+			  </div>
+			</div><!-- col-md-6 -->
+
+
+		</div>
+
+	</div>
+
+</div>
+
+
+
+<!--  Jumbotron What others say about healthhub -->
+
+<!-- ****************************************   contact us ************************************************-->
+
+
+<div style="background-color:#fff;">
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-6">
+
+
+			<div class="row">
+				<div class="col-sm-12" style="text-align:center;">
+					<img src="img/mail.png" alt="" >
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-12" style="text-align:center; margin-bottom: 40px;">
+					<h4 style="font-family: 'Abel'; color:#567;">Leave us a message</h4>
+				</div>
+			</div>
+			<div class="row">
+				<form class="form-horizontal" role="form" action= "<?php echo $_SERVER['PHP_SELF'] ?>" method = "POST">
+				  <div class="form-group">
+				    <label class = "control-label col-sm-2" for="name">Name :</label>
+				    <div class="col-sm-8">
+				      <input type="text" name = "name" class="form-control" id="name" placeholder="Name" required>
+				    </div>
+				  </div>
+
+				  <div class="form-group">
+				  	<label class = "control-label col-sm-2" for="email">Email :</label>
+				    <div class="col-sm-8">
+				      <input type="email" name = "email" class="form-control" id="inputEmail" placeholder="Email" required>
+				    </div>
+				  </div>
+
+				  <div class="form-group">
+				  	<label class = "control-label col-sm-2" for="phone_no">Mobile :</label>
+				    <div class="col-sm-8">
+				      <input type="number" name = "phone_no" class="form-control" id="phone_no" placeholder="Mobile no">
+				    </div>
+				  </div>
+
+				  <div class="form-group">
+				  	<label class = "control-label col-sm-2" for="found_us_by">Found in:</label>
+				  	<div class="col-sm-8">
+				  		<select class = "form-control" name="found_us_by" id="found_us_by">
+				  			<option value="Google+">Google+</option>
+				  			<option value="Facebook">Facebook</option>
+				  			<option value="Twitter">Twitter</option>
+				  			<option value="Newspaper">Newspaper</option>
+				  			<option value="Youtube">Youtube</option>
+				  			<option value="Friend">Friend</option>
+
+				  		</select>
+				  	</div>
+				  </div>
+				  <div class="form-group">
+					<label class = "control-label col-sm-2" for="message">Message :</label>
+				    <div class="col-sm-8">
+						<textarea class="form-control" rows="5" placeholder = "Message" name = "message" required></textarea>
+				    </div>
+				  </div>
+				  <div class="form-group">
+
+				    <div class="col-sm-4 col-sm-offset-2">
+				      <button type="submit" name = "submit" value = "send" class="button is-success">SEND MESSAGE</button>
+				    </div>
+				  </div>
+				</form>
+			</div>
+		</div><!-- end contact form -->
+
+
+		<div class="col-md-6">
+
+			<div class="row">
+				<div class="col-sm-12" style="text-align:center;">
+					<img src="img/compass.png" alt="" >
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-12" style="text-align:center; margin-bottom: 40px;">
+					<h4 style="font-family: 'Abel'; color:#567;">Locate us</h4>
+				</div>
+			</div>
+			<div class="row">
+
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2480.069293959975!2d-0.1189968999999727!3d51.56696319999992!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48761ba2b61888cf%3A0x5fcddd4f8a152c3e!2sLondon+N4+3BN!5e0!3m2!1sen!2suk!4v1399078954870" width="100%" height="310" frameborder="2" style="border:0px; /*box-shadow: 2px 2px 5px #888888;*/"></iframe>
+			</div>
+		</div><!-- end google map -->
+
+	</div><!-- end row -->
+</div><!-- container end -->
+</div>
+
+
+<!-- *************************************************  contact us end  *****************************************************-->
+
+<!-- Be a Hero -->
+
+
+
+<div class="jumbotron hero" id = "registration">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12" >
+
+				<h1>Be A Hero </h1>
+				<h4 >The finest gesture you can make is to save life by donating Blood</h4>
+				<img class = "img-circle" src="img/hero.jpg" alt="" style="height:150px;">
+
+			</div><!-- col-sm-12 -->
+		</div><!-- row -->
+		<div class="row">
+			<div class="col-sm-6">
+				<form class="form-horizontal" role="form"  action="#">
+					<div class="col-sm-8 col-sm-offset-4">
+
+						<div class="form-group">
+						<input class = "form-control" type="text" placeholder = "First Name" >
+						</div>
+						<div class="form-group">
+						<input class = "form-control" type="text" placeholder = "Last Name ">
+						</div>
+						<div class="form-group">
+						<input class = "form-control" type="text" placeholder = "Email ">
+						</div>
+						<div class="form-group">
+						<input class = "form-control" type="password" placeholder = "Password ">
+						</div>
+						<div class="form-group">
+						<input class = "form-control" type="password" placeholder = "Confirm Password ">
+						</div>
+						<div class="form-group">
+						<input class = "form-control" type="text" placeholder = "Date of Birth">
+						</div>
+					</div>
+			</div>
+			<div class="col-sm-6">
+					<div class="col-sm-8 ">
+
+						<div class="form-group">
+						<input class = "form-control" type="text" placeholder = "Mobile No ">
+						</div>
+						<div class="form-group">
+						<input class = "form-control" type="text" placeholder = "Address">
+						</div>
+						<div class="form-group">
+						<input class = "form-control" type="text" placeholder = "City">
+						</div>
+						<div class="form-group">
+						<input class = "form-control" type="text" Placeholder = "ZIP">
+						</div>
+						<div class="form-group">
+						<input class = "form-control" type="text" Placeholder = "Blood group">
+						</div>
+						<div class="form-group">
+						<input class = "form-control" type="file" Placeholder = "Image">
+						</div>
+
+					</div><!-- col-sm-8 -->
+			</div><!-- col-sm-6 -->
+					<div class="col-sm-4 col-sm-offset-4">
+						<div class="form-group">
+						<input class=" form-control button is-danger" type="button" name="submit" value="Register as a donor">
+						</div>
+					</div>
+				</form><!-- form end -->
+
+		</div><!-- row -->
+	</div><!-- container -->
+</div><!-- jumbotron -->
+
+
+
+<!-- ************************ -->
+
+<!-- footer -->
+
+<div class="navbar navbar-default navbar-static-bottom" role="navigation">
+
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+				<p class="navbar-text pull-left">copyright © 2017 <a href="http://www.dartsling.com"> Dartsling</a></p>
+			</div>
+			<div class="col-md-4 col-md-offset-4">
+				<a href="#">
+					<span class="fa-stack fa-lg pull-right">
+					  <i class="fa fa-square-o fa-stack-2x"></i>
+					  <i class="fa fa-twitter fa-stack-1x"></i>
+					</span>
+				</a>
+
+				<a href="#">
+					<span class="fa-stack fa-lg pull-right">
+					  <i class="fa fa-square-o fa-stack-2x"></i>
+					  <i class="fa fa-facebook fa-stack-1x"></i>
+					</span>
+				</a>
+
+				<a href="#">
+					<span class="fa-stack fa-lg pull-right">
+					  <i class="fa fa-square-o fa-stack-2x"></i>
+					  <i class="fa fa-google-plus fa-stack-1x"></i>
+					</span>
+				</a>
+			</div>
+		</div>	<!-- row -->
+	</div><!-- container -->
+</div>
+
+
+<!-- footer -->
+
+
+
+
+<!-- ALL JAVASCRIPT -->
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/carousel.js"></script>
+
+<!-- ALL JAVASCRIPT -->
+</body>
+</html>
